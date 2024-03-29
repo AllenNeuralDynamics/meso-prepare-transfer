08947bf928307f32bd3622c96bae8849cbb4a21c
Arielle Leon <ariellel@alleninstitute.org>
2024-03-29 15:32:29 -0700

    unit test edits

--- src/aind_mesoscope_user_schema_ui/user_settings_ui.py
+++ src/aind_mesoscope_user_schema_ui/user_settings_ui.py
@@ -5,21 +5,18 @@ from pathlib import Path
 import yaml
 import json
 import os
-
-from PySide6.QtCore import QMetaMethod
+from typing import Union
 from PySide6.QtWidgets import QApplication, QWidget
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py
-from ui.ui_form import UiUserSettings
-from config import UserInput
+from aind_mesoscope_user_schema_ui.ui.ui_form import UiUserSettings
+from aind_mesoscope_user_schema_ui.config import UserInput
 
 
 class Widget(QWidget):
     _LIMS_URLS = {
-        "user": "http://lims2/users.json?login={}",
-        "donor": "http://lims2/donors/info/details.json?external_donor_name={}&parent_specimens=true",
         "session": "http://lims2/ophys_sessions.json?id={}",
     }
 
@@ -29,8 +26,7 @@ class Widget(QWidget):
         self.ui.setupUi(self)
         self.connect_signals()
         config_fp = os.getenv("MESO_USER_SETTING_CONFIG")
-        with open(config_fp, "r") as file:
-            self.config = yaml.safe_load(file)
+        self.config = self.get_config(config_fp)
 
     def connect_signals(self) -> None:
         """Connect signals to slots."""
@@ -38,6 +34,22 @@ class Widget(QWidget):
         self.ui.submitPushButton.clicked.connect(self.submit_button_clicked)
         self.ui.userNameLineEdit.textChanged.connect(self.check_submit_button)
         self.ui.sessionIdLineEdit.textChanged.connect(self.check_submit_button)
+
+    def get_config(self, file_path: str) -> dict:
+        """loads and returns yaml file contents as dict
+
+        Parameters
+        ----------
+        file_path : str
+            path to configuration
+
+        Returns
+        -------
+        dict
+            config data
+        """
+        with open(file_path, "rb") as file:
+            return yaml.safe_load(file)
 
     def _get_lims_response(self, lims_entry: str, api: str) -> requests.Response:
         """Returns lims response.
@@ -51,51 +63,111 @@ class Widget(QWidget):
         -------
         requests.Response
         """
-        return requests.get(self._LIMS_URLS[api].format(lims_entry))
+        return requests.get(self._LIMS_URLS[api].format(lims_entry), timeout=2)
 
-    def _get_lims_project_code(self, lims_response: list) -> str:
-        """Retrieves project code from LIMS response.
+    def _check_user(self, user_name: str) -> bool:
+        """verify there are two components to the string of the user input
 
         Parameters
         ----------
-        lims_response : list
-            LIMS response.
+        user_name: str
+            User name from line edit
 
         Returns
         -------
-        str
-            Project code.
+        bool
+            truth
         """
-        print(f"Getting LIMS project code from response: {lims_response}")
-
-    def submit_button_clicked(self) -> None:
-        """Runs job to retrieve data from user inputs.
-        If successful will drop a UserSettings.json in the acquisition directory."""
-        print("Submit button clicked")
-        user_name = self.ui.userNameLineEdit.text()
-        session_id = self.ui.sessionIdLineEdit.text()
         if not " " in user_name:
-            self.ui.error_message.showMessage("Enter a valid user name")
-            return
-        session_response = self._get_lims_response(session_id, "session")
+            return False
+        else:
+            return True
+
+    def _session_data(self, session_response: requests.Response) -> Union[bool, dict]:
+        """Validates lims session
+
+        Parameters
+        ----------
+        session_resposne: requests.Response
+
+        Returns
+        -------
+        Union[bool, dict]
+            session data or resposne
+        """
         session_data = session_response.json()[0]
         try:
             assert session_data
+            return session_data
         except AssertionError:
-            self.ui.error_message.showMessage("Invalid session ID")
-            return
-        subject_id = session_data["specimen"]["external_specimen_name"]
-        camera_json = [c for c in Path(self.config["behavior_dir"]).glob(f"{session_id}*.json")]
-        if len(camera_json) < 3:
-            self.ui.error_message.showMessage("Less than 3 camera jsons found")
-        if len(camera_json) == 0:
-            self.ui.error_message.showMessage("No camera metadata found: contact engineering")
-            return
-        with open(camera_json[0], "r") as j:
-            behavior_data = json.load(j)
-        start_time = behavior_data["RecordingReport"]["TimeStart"]
-        end_time = behavior_data["RecordingReport"]["TimeEnd"]
+            return False
 
+    def _project_and_mouse_id(self, session_data: dict) -> tuple:
+        """returns project id and mouse id from session data
+
+        Parameters
+        ----------
+        session_data : dict
+            session data from lims response
+
+        Returns
+        -------
+        tuple
+            (subject id, project id)
+        """
+        subject_id = session_data["specimen"]["external_specimen_name"]
+        project_id = session_data["project"]["code"]
+        return subject_id, project_id
+
+    def _behavior_cameras(self, session_id: str) -> Union[bool, dict]:
+        """_summary_
+
+        Parameters
+        ----------
+        session_id : str
+            session id to glob for metadata
+
+        Returns
+        -------
+        Union[bool, dict]
+            camera metadata or false
+        """
+        camera_json = [
+            c for c in Path(self.config["behavior_dir"]).glob(f"{session_id}*.json")
+        ]
+        if len(camera_json) < 3:
+            print("Less than 3 camera jsons found")
+        if len(camera_json) == 0:
+            return False
+        with open(camera_json[0], "rb") as j:
+            return json.load(j)
+
+    def _generate_user_settings(
+        self,
+        start_time: str,
+        end_time: str,
+        subject_id: str,
+        project_id: str,
+        session_id: str,
+        user_name: str,
+    ) -> None:
+        """_Generates user settings json for metdata mapping into session json
+
+        Parameters
+        ----------
+        start_time : str
+            acquisition start time (according to behavior cams)
+        end_time : str
+            acquisition end time (according to behavior cams)
+        subject_id : str
+            mouse id
+        project_id : str
+            project associated with session
+        session_id : str
+            session id
+        user_name: str
+            full name of user
+        """
         user_input = UserInput(
             input_source=self.config["acquisition_dir"],
             behavior_source=self.config["behavior_dir"],
@@ -103,24 +175,50 @@ class Widget(QWidget):
             session_start_time=start_time,
             session_end_time=end_time,
             subject_id=subject_id,
-            project=session_response.json()[0]["project"]["code"],
+            project=project_id,
             experimenter_full_name=[user_name],
         )
         user_input = user_input.dict()
         with open(
-            Path(self.config["acquisition_dir"]) / session_id / "user_settings.json", "w"
+            Path(self.config["acquisition_dir"]) / session_id / "user_settings.json",
+            "w",
         ) as j:
             json.dump(user_input, j, indent=4)
+
+    def submit_button_clicked(self) -> None:
+        """Runs job to retrieve data from user inputs.
+        If successful will drop a UserSettings.json in the acquisition directory.
+        """
+        print("Submit button clicked")
+        user_name = self.ui.userNameLineEdit.text()
+        session_id = self.ui.sessionIdLineEdit.text()
+        if not self._check_user(user_name):
+            self.ui.error_message.showMessage("Enter a valid user name")
+            return
+        session_response = self._get_lims_response(session_id, "session")
+        session_data = self._session_data(session_response)
+        if not session_data:
+            self.ui.error_message.showMessage("Invalid session ID")
+            return
+        subject_id, project_id = self._project_and_mouse_id(session_data)
+        behavior_data = self._behavior_cameras(session_id)
+        if not behavior_data:
+            self.ui.error_message.showMessage(
+                "No camera metadata found: contact engineering"
+            )
+            return
+        start_time = behavior_data["RecordingReport"]["TimeStart"]
+        end_time = behavior_data["RecordingReport"]["TimeEnd"]
+        self._generate_user_settings(
+            start_time, end_time, subject_id, project_id, session_id, user_name
+        )
         self.ui.error_message.showMessage("User settings saved")
         self.ui.userNameLineEdit.clear()
         self.ui.sessionIdLineEdit.clear()
 
     def check_submit_button(self) -> None:
         """Checks conditions to see if the submit button can be enabled."""
-        if (
-            self.ui.userNameLineEdit.text()
-            and self.ui.sessionIdLineEdit.text()
-        ):
+        if self.ui.userNameLineEdit.text() and self.ui.sessionIdLineEdit.text():
             self.ui.submitPushButton.setEnabled(True)
         else:
             self.ui.submitPushButton.setEnabled(False)
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,10 +1,19 @@
 """Example test template."""
 
+from unittest.mock import MagicMock, patch, mock_open
 import unittest
+from aind_mesoscope_user_schema_ui.user_settings_ui import Widget
+from requests import Response
+import json
+import sys
+from PySide6.QtWidgets import QApplication
 
 # What I want to test:
-# Whether the submit button is enabled or disabled based on the textChanged signal from the line edits.
+# Whether the submit button is enabled or disabled based on the 
+# textChanged signal from the line edits.
 # Test if the json outputs as expected
+app = QApplication(sys.argv)
+
 
 class UserSettingsTest(unittest.TestCase):
     """Example Test Class"""
@@ -17,11 +26,112 @@ class UserSettingsTest(unittest.TestCase):
         cls.user_name_bad = "Jareth"
         cls.session_id_bad = "0987654321"
         cls.subject_id_bad = "900001"
+        cls.project = "mouse"
+        cls.lims_response = [
+            {
+                "project": {"code": "omFISHSstMeso"},
+                "specimen": {"external_specimen_name": "711914"},
+            }
+        ]
+        cls.config = {
+            "acquisition_dir": "D:\\mesoscope_data",
+            "behavior_dir": "D:\\behavior",
+            "output_dir": ".",
+        }
 
-    def test_name_input(self):
-        """Example of how to test the truth of a statement."""
+    # mocked response borrowed from Jon Y. 
+    #   https://github.com/AllenNeuralDynamics/aind-data-access-api/blob/f522f80a3a210abc276a565fd4e3fdb5d18068be/tests/test_document_db.py#L41
+    @patch("os.getenv")
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data="{ \
+           'acquisition_dir': 'D:\\mesoscope_data', \
+            'output_dir': '.' \
+            }",
+    )
+    @patch("requests.get")
+    def test_lims_response(
+        self,
+        mock_get: MagicMock,
+        mock_file_open: MagicMock,
+        mock_env: MagicMock,
+    ):
+        """test lims response."""
+        mock_env.return_value = "/path/config.yml"
+        w = Widget()
 
-        self.assertTrue(1 == 1)
+        mock_response = Response()
+        mock_response.status_code = 200
+        body = json.dumps(self.lims_response)
+        mock_response._content = body.encode("utf-8")
+        mock_get.return_value = mock_response
+        response = w._get_lims_response(self.session_id, "session")
+        self.assertEqual(response.json(), self.lims_response)
+
+        mock_response = Response()
+        mock_response.status_code = 404
+        body = json.dumps(None)
+        mock_response._content = json.dumps({"body": body}).encode("utf-8")
+        mock_get.return_value = mock_response
+        response = w._get_lims_response(self.session_id_bad, "session")
+        self.assertEqual(response.status_code, 404)
+        w.close()
+
+    @patch("os.getenv")
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data="{ \
+           'acquisition_dir': 'D:\\mesoscope_data', \
+            'output_dir': '.' \
+            }",
+    )
+    def test_user_response(
+        self,
+        mock_file_open: MagicMock,
+        mock_env: MagicMock,
+    ):
+        """Test user response"""
+        mock_env.return_value = "/path/config.yml"
+        w = Widget()
+
+        good_response = w._check_user(self.user_name)
+        bad_response = w._check_user(self.user_name_bad)
+        self.assertEqual(good_response, True)
+        self.assertEqual(bad_response, False)
+        w.close()
+
+    @patch("os.getenv")
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data="{ \
+           'acquisition_dir': 'D:\\mesoscope_data', \
+            'output_dir': '.' \
+            }",
+    )
+    @patch("requests.get")
+    def test_session_data(
+        self,
+        mock_get: MagicMock,
+        mock_file_open: MagicMock,
+        mock_env: MagicMock,
+    ):
+        """Test session data"""
+        mock_env.return_value = "/path/config.yml"
+        w = Widget()
+
+        mock_response = Response()
+        mock_response.status_code = 200
+        body = json.dumps(self.lims_response)
+        mock_response._content = body.encode("utf-8")
+        mock_get.return_value = mock_response
+        response = w._get_lims_response(self.session_id, "session")
+        session_data = w._session_data(response)
+        self.assertEqual(session_data, self.lims_response[0])
+
+        w.close()
 
 
 if __name__ == "__main__":
