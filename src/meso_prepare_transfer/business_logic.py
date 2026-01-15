4fe28edd60f58d1dac3409a8c3a408bd993dc41b
Patrick Latimer <patrick.latimer@alleninstitute.org>
2026-01-15 12:14:46 -0800

    Fix bad project name in watchdog manifest, remove old commented code

--- src/meso_prepare_transfer/business_logic.py
+++ src/meso_prepare_transfer/business_logic.py
@@ -2,7 +2,6 @@
 
 from datetime import datetime, timedelta
 
-# import requests
 from pathlib import Path
 import json
 
@@ -19,31 +18,6 @@ from aind_metadata_mapper.mesoscope.session import MesoscopeEtl
 
 from meso_prepare_transfer.config import Config
 from meso_prepare_transfer.utils.sync_dataset import Sync
-
-
-# # TODO: Remove
-# @logger.catch()
-# def query_lims(
-#     session_id: str,
-#     session_endpoint: str,
-# ) -> tuple[str, str]:
-#     """Query LIMS for subject and project ID based on session ID.
-
-#     Raises:
-#         requests.exceptions.RequestException: If the HTTP request fails.
-#         IndexError: If the session id is invalid and no data is found in LIMS.
-#         KeyError: If the LIMS session data isn't formatted as expected.
-#     """
-#     url = session_endpoint.format(session_id)
-#     response = requests.get(url, timeout=2)
-#     response.raise_for_status()
-#     try:
-#         session_data = response.json()[0]
-#     except IndexError:
-#         raise IndexError("No data found for the given session ID.")
-#     subject_id = session_data["specimen"]["external_specimen_name"]
-#     project_id = session_data["project"]["code"]
-#     return subject_id, project_id
 
 
 @logger.catch()
@@ -95,6 +69,7 @@ def generate_aind_metadata(
     user_name: str,
     subject_id: str,
     project_id: str,
+    data_schema_project_name: str,
     start_time: str,
     end_time: str,
     config: Config,
@@ -116,13 +91,6 @@ def generate_aind_metadata(
     )
     meso_etl = MesoscopeEtl(session_metadata)
     meso_etl.run_job()
-
-    # session_json_contents = session_metadata.model_dump()
-
-    if "OpenScope" in project_id:
-        data_schema_project_name = "OpenScope"
-    else:
-        data_schema_project_name = "Learning mFISH-V1omFISH"
 
     investigators = [PIDName(name=inv) for inv in config.investigators[data_schema_project_name]]
 
@@ -185,11 +153,7 @@ def generate_watchdog_manifest(
     """Generates a manifest file for aind-watchdog-service to transfer data off-rig"""
     logger.info("Generating manifest file")
     data_directory = config.acquisition_dir / session_id
-    # sync_file = [i for i in data_directory.glob("*.h5") if "full_field" not in str(i)][0]
-    # start_time, _ = self.get_start_end_times(sync_file)
-    # name = platform.replace("_", "-") + "_" + user_input["subject_id"] + "_" + start_time.strftime("%Y-%m-%d_%H-%M-%S")
-    # acquisition_dir = Path(user_input["input_source"])
-    # behavior_dir = Path(user_input["behavior_source"])
+
     data = {}
     data[Modality.POPHYS.abbreviation] = search_files(data_directory, config.modalities["pophys"])
     data[Modality.BEHAVIOR.abbreviation] = search_files(data_directory, config.modalities["behavior"])
@@ -198,9 +162,6 @@ def generate_watchdog_manifest(
         config.modalities["behavior-videos"],
         extra_search_key=data_directory.name,
     )
-    # if self.error:
-    #     self.ui.error_message.showMessage(f"Files not found: {self.error}")
-    #     self.error = []
     schemas = []
     for i in config.schemas:
         # If schema is not a file, that exists, pull it from the data directory
@@ -226,17 +187,10 @@ def process_dataset(user_name: str, session_id: str, config: Config) -> bool:
     """Process a single dataset: generate metadata and manifest files."""
     logger.info("Processing dataset", session_id=session_id, user_name=user_name)
 
-    # subject_id, project_id = query_lims(session_id, config.session_endpoint)
     subject_id, project_id = parse_platform_json(config.acquisition_dir / session_id)
 
     logger.info(f"Project ID: {project_id}, Subject ID: {subject_id}")
 
-    # behavior_data = behavior_camera_data(session_id, Path(config.behavior_dir))
-    # behavior_data = self._behavior_cameras(session_id)
-    # if not behavior_data:
-    #     logger.error("No camera metadata found: contact engineering")
-    #     self.ui.error_message.showMessage("No camera metadata found: contact engineering")
-    #     return
     data_directory = config.acquisition_dir / session_id
 
     camera_jsons = list(config.behavior_video_dir.glob(f"{session_id}*.json"))
@@ -248,6 +202,11 @@ def process_dataset(user_name: str, session_id: str, config: Config) -> bool:
 
     start_time, end_time = get_start_end_times(data_directory)
 
+    if "OpenScope" in project_id:
+        data_schema_project_name = "OpenScope"
+    else:
+        data_schema_project_name = "Learning mFISH-V1omFISH"
+
     generate_aind_metadata(
         data_directory=data_directory,
         behavior_dir=Path(config.behavior_video_dir),
@@ -255,6 +214,7 @@ def process_dataset(user_name: str, session_id: str, config: Config) -> bool:
         user_name=user_name,
         subject_id=subject_id,
         project_id=project_id,
+        data_schema_project_name=data_schema_project_name,
         start_time=start_time,
         end_time=end_time,
         config=config,
@@ -263,7 +223,7 @@ def process_dataset(user_name: str, session_id: str, config: Config) -> bool:
     generate_watchdog_manifest(
         session_id,
         subject_id,
-        project_id,
+        data_schema_project_name,
         user_name,
         start_time,
         config,
